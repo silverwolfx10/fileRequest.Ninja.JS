744ce41607214025881b899214aa7a50be9394a6
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2015-08-31 17:11:24 +0000

    refatoracao

--- fileRequest.js
+++ fileRequest.js
@@ -12,46 +12,51 @@
  * 
  */
 this.Ninja.module('$fileRequest', ['$curry', '$http'], function ($curry, $http) {
-
-
-  // TODO: Refatorar :@
-
-  var handler = {};
   
-  return $curry(function (url, callback) {
-    
-    if (!!handler[url]) {
-      return void handler[url](url, callback);
-    }
-    
-    if (!!localStorage.getItem(url)) {
-      
-      handler[url] = function (url, callback) {
-        callback(localStorage.getItem(url));
-      };
-      
-      return void callback(localStorage.getItem(url));
-      
-    }
-    
-    var listeners = [callback];
-    
-    handler[url] = function (url, callback) {
-      listeners.push(callback);
+  var listeners = {};
+  var state = {};
+
+  function requestFile(url) {
+    $http('GET', url, '')
+      .when(200, $curry(savingResponseInTheStorage)(url))
+      .when(200, $curry(setTheStateForActive)(url))
+      .when(200, $curry(runQueue)(url));
+  }
+  
+  function runQueue(url) {
+    listeners[url].forEach($curry(state)(url));
+  }
+  
+  function fileRequest(url, callback) {
+    solveCurrentState(url);
+    state(url, callback);
+  }
+  
+  function setTheStateForWaiting(url, callback) {
+    state[url] = function (url, callback) {
+      (listeners[url] = listeners[url] || []).push(callback);
     };
+  }
+  
+  function savingResponseInTheStorage(url, xhr) {
+    localStorage.setItem(url, xhr.responseText);
+  }
+  
+  function solveCurrentState(url) {
     
-    $http('get', url, '').when(200, function (xhr) {
-      
-      localStorage.setItem(url, xhr.responseText);
-      
-      handler[url] = function (url, callback) {
-        callback(xhr.responseText);
-      };
-      
-      listeners.forEach(handler[url].bind(null, null));
-      
-    });
+    if (state[url]) return;
     
-  });
+    setTheStateForWaiting();
+    requestFile(url);
+    
+  }
+  
+  function setTheStateForActive(url) {
+    state[url] = function (url, callback) {
+      callback(localStorage.getItem(url));
+    };
+  }
+  
+  return $curry(fileRequest);
 
 });
